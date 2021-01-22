4dca62d0f7e4215ca8f283d57d47119016500a2d
Chelsea Becker <cschweig2@gmail.com>
2021-01-22 09:08:58 -0800

    add Parks.Models directive

--- Parks/Controllers/ParksController.cs
+++ Parks/Controllers/ParksController.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parks.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Parks.Controllers
 {
