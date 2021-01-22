8e95e502f85a1c2fa5d992c191ebf5517842ecf6
Chelsea Becker <cschweig2@gmail.com>
2021-01-22 10:32:17 -0800

    add Swagger specs

--- ParkFinder/Startup.cs
+++ ParkFinder/Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ParkFinder.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace ParkFinder
 {
@@ -26,7 +30,27 @@ namespace ParkFinder
                 opt.UseInMemoryDatabase("Parks")); //for swagger
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Park Finder API",
+                    Description = "An ASP.NET Core Web API to find national and state parks in your area",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Chelsea Becker",
+                        Email = "chelraebecker@gmail.com",
+                        Url = new Uri("https://github.com/cschweig2/ParkFinder.Solution"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +65,8 @@ namespace ParkFinder
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
 
             app.UseSwaggerUI(c =>
             {
