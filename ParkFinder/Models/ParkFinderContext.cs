dfd46aec6ae25717c72b260506631723b0041fc2
Chelsea Becker <cschweig2@gmail.com>
2021-01-22 09:55:35 -0800

    correct typo in seed data

--- ParkFinder/Models/ParkFinderContext.cs
+++ ParkFinder/Models/ParkFinderContext.cs
@@ -13,8 +13,8 @@ namespace ParkFinder.Models
         {
             builder.Entity<Park>()
                 .HasData(
-                    new Park { ParkId = 1, ParkType = "National Park", ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Webside = "https://www.nps.gov/crla/index.htm"},
-                    new Park { ParkId = 2, ParkType = "National Reserve", ParkName = "New Jersey Pinelands", City = "Batsto", State = "NJ", Status = "Open", Webside = "https://www.nps.gov/pine/index.htm"}
+                    new Park { ParkId = 1, ParkType = "National Park", ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Website = "https://www.nps.gov/crla/index.htm"},
+                    new Park { ParkId = 2, ParkType = "National Reserve", ParkName = "New Jersey Pinelands", City = "Batsto", State = "NJ", Status = "Open", Website = "https://www.nps.gov/pine/index.htm"}
                 );
         }
     }
