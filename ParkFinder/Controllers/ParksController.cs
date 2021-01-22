6116f6572e98a9ef6d166fa530800fb08b1097a7
Chelsea Becker <cschweig2@gmail.com>
2021-01-22 14:33:31 -0800

    add random route to V2

--- ParkFinder/Controllers/ParksController.cs
+++ ParkFinder/Controllers/ParksController.cs
@@ -8,13 +8,14 @@ using System.Threading.Tasks;
 
 namespace ParkFinder.Controllers
 {
+    [ApiVersion("1.0", Deprecated = true)]
     [Route("api/parks")]
     [ApiController]
-    public class ParksController : ControllerBase
+    public class ParksV1Controller : ControllerBase
     {
         private ParkFinderContext _db;
 
-        public ParksController(ParkFinderContext db)
+        public ParksV1Controller(ParkFinderContext db)
         {
             _db = db;
         }
@@ -73,6 +74,84 @@ namespace ParkFinder.Controllers
             var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
             _db.Parks.Remove(parkToDelete);
             _db.SaveChanges();
+        }
+
+    }
+
+    [ApiVersion("2.0")]
+    [Route("api/{v:ApiVersion}/parks")]
+    [ApiController]
+    public class ParksV2Controller : ControllerBase
+    {
+        private ParkFinderContext _db;
+
+        public ParksV2Controller(ParkFinderContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> Get(string parkType, string parkName, string city, string state, string status)
+        {
+            var query = _db.Parks.AsQueryable();
+            if (parkType != null)
+            {
+                query = query.Where(entry => entry.ParkType == parkType);
+            }
+            if (parkName != null)
+            {
+                query = query.Where(entry => entry.ParkName == parkName);
+            }
+            if (city != null)
+            {
+                query = query.Where(entry => entry.City == city);
+            }
+            if (state != null)
+            {
+                query = query.Where(entry => entry.State == state);
+            }
+            if (status != null)
+            {
+                query = query.Where(entry => entry.Status == status);
+            }
+            return query.ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+        }
+
+        [HttpGet("random")]
+        public ActionResult<Park> Get()
+        {
+            var rand = new Random();
+            var random = rand.Next(1, _db.Parks.Count());
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == random);
         }
 
     }
