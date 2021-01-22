a3e4bcef825a30ae2e15a0ba9bad319a30712bd4
Chelsea Becker <cschweig2@gmail.com>
2021-01-22 09:54:34 -0800

    add seeding data

--- ParkFinder/Models/ParkFinderContext.cs
+++ ParkFinder/Models/ParkFinderContext.cs
@@ -9,12 +9,13 @@ namespace ParkFinder.Models
         }
         public DbSet<Park> Parks { get; set; }
 
-        // protected override void OnModelCreating(ModelBuilder builder)
-        // {
-        //     builder.Entity<Park>()
-        //         .HasData(
-        //             new Park { ParkId = 1, ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Webside = "https://www.nps.gov/crla/index.htm"},
-        //             new Park { ParkId = 1, ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Webside = "https://www.nps.gov/crla/index.htm"}
-        // }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>()
+                .HasData(
+                    new Park { ParkId = 1, ParkType = "National Park", ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Webside = "https://www.nps.gov/crla/index.htm"},
+                    new Park { ParkId = 2, ParkType = "National Reserve", ParkName = "New Jersey Pinelands", City = "Batsto", State = "NJ", Status = "Open", Webside = "https://www.nps.gov/pine/index.htm"}
+                );
+        }
     }
 }
