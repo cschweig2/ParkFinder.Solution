b212d2d1ef2275be6009fddfd5703fab4e39b86d
Chelsea Becker <cschweig2@gmail.com>
2021-01-24 20:03:14 -0800

    add user to context

--- ParkFinder/Models/ParkFinderContext.cs
+++ ParkFinder/Models/ParkFinderContext.cs
@@ -8,6 +8,7 @@ namespace ParkFinder.Models
         {
         }
         public DbSet<Park> Parks { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -15,6 +16,11 @@ namespace ParkFinder.Models
                 .HasData(
                     new Park { ParkId = 1, ParkType = "National Park", ParkName = "Crater Lake", City = "Crater Lake", State = "OR", Status = "Open", Website = "https://www.nps.gov/crla/index.htm"},
                     new Park { ParkId = 2, ParkType = "National Reserve", ParkName = "New Jersey Pinelands", City = "Batsto", State = "NJ", Status = "Open", Website = "https://www.nps.gov/pine/index.htm"}
+                );
+
+            builder.Entity<User>()
+                .HasData(
+                    new User { UserId = 3, FirstName = "test", LastName = "user", Username = "admin", Password = "test"}
                 );
         }
     }
